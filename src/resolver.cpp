989bad92a7fa9210229e500738cccacb6b06e27c
RVitalicS <rvitalics@gmail.com>
2021-11-02 17:51:40 +0300

    override private _Cache inherited from ArDefaultResolver

--- src/resolver.cpp
+++ src/resolver.cpp
@@ -1,11 +1,14 @@
 
 #include "resolver.h"
 
-#include "pxr/base/tf/pathUtils.h"
-#include "pxr/base/tf/fileUtils.h"
+#include <pxr/base/tf/getenv.h>
+#include <pxr/base/tf/fileUtils.h>
+
 #include <pxr/usd/ar/defineResolver.h>
+#include <pxr/usd/ar/filesystemAsset.h>
 
 #include "debugCodes.h"
+#include <tbb/concurrent_hash_map.h>
 
 
 PXR_NAMESPACE_OPEN_SCOPE
@@ -14,8 +17,61 @@ AR_DEFINE_RESOLVER(EnvResolver, ArResolver);
 
 
 
-static std::string 
-_EnvResolve(const std::string& path)
+struct EnvResolver::_Cache
+{
+    using _PathToResolvedPathMap = 
+        tbb::concurrent_hash_map<std::string, std::string>;
+    _PathToResolvedPathMap _pathToResolvedPathMap;
+};
+
+EnvResolver::_CachePtr 
+EnvResolver::_GetCurrentCache() {
+    return _threadCache.GetCurrentCache();
+}
+
+std::string
+EnvResolver::ResolveWithAssetInfo(
+    const std::string& path, 
+    ArAssetInfo* assetInfo)
+{
+
+    if (path.empty()) {
+        return path;
+    }
+
+    if (_CachePtr currentCache = _GetCurrentCache()) {
+        _Cache::_PathToResolvedPathMap::accessor accessor;
+        if (currentCache->_pathToResolvedPathMap.insert(
+                accessor, std::make_pair(path, std::string()))) {
+            accessor->second = _EnvResolve(path);
+        }
+        return accessor->second;
+    }
+
+    return _EnvResolve(path);
+}
+
+
+
+std::string
+EnvResolver::_EnvResolve(const std::string& path)
+{
+    TF_DEBUG(USD_ENV_RESOLVER)
+        .Msg("Resolve : %s\n", path.c_str());
+
+    std::string
+    resolvedPath = EnvResolve(path);
+
+    if (!TfPathExists(resolvedPath)) {
+        resolvedPath = std::string();
+    }
+
+    return resolvedPath;
+}
+
+
+std::string
+EnvResolver::EnvResolve(const std::string& path)
 {
     if( path.find( "${" ) == std::string::npos ) return path;
 
@@ -25,30 +81,15 @@ _EnvResolve(const std::string& path)
     if( post.find( '}' ) == std::string::npos ) return path;
 
     std::string variable = post.substr( 0, post.find( '}' ) );
-    std::string value    = "";
-
     post = post.substr( post.find( '}' ) + 1 );
 
-    const char *var = std::getenv( variable.c_str() );
-    if( var != nullptr ) value = std::string( var );
+    std::string env = TfGetenv(variable);
+    std::string value;
+    if (!env.empty()) value = env;
 
-    return _EnvResolve( pre + value + post );
+    return EnvResolve( pre + value + post );
 }
 
-
-
-ArResolvedPath
-EnvResolver::_Resolve(const std::string& path) const
-{
-    TF_DEBUG(USD_ENV_RESOLVER)
-        .Msg("EnvResolver input: %s\n", path.c_str());
-
-    std::string resolvedPath = _EnvResolve(path);
-
-    return TfPathExists(resolvedPath)
-        ? ArResolvedPath(TfAbsPath(resolvedPath))
-        : ArResolvedPath();
-}
 
 
 
--- src/resolver.h
+++ src/resolver.h
@@ -1,9 +1,7 @@
 #pragma once
 
 #include <pxr/usd/ar/defaultResolver.h>
-#include <pxr/usd/ar/resolvedPath.h>
 
-#include <string>
 
 
 PXR_NAMESPACE_OPEN_SCOPE
@@ -12,8 +10,26 @@ class EnvResolver
      : public ArDefaultResolver
 {
 public:
-    ArResolvedPath _Resolve(
-        const std::string& path) const override;
+
+    std::string
+    EnvResolve(const std::string& path);
+
+    std::string
+    ResolveWithAssetInfo(
+        const std::string& path, 
+        ArAssetInfo* assetInfo) override;
+
+private:
+    
+    struct _Cache;
+    using _PerThreadCache = ArThreadLocalScopedCache<_Cache>;
+    _PerThreadCache _threadCache;
+
+    using _CachePtr = _PerThreadCache::CachePtr;
+    _CachePtr _GetCurrentCache();
+
+    std::string
+    _EnvResolve(const std::string& path);
 };
 
 PXR_NAMESPACE_CLOSE_SCOPE
